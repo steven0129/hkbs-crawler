bc966f6aa03270d3a3e609f0b2171ba54481433e
Administrator <h3584935@gmail.com>
2018-03-06 18:23:29 +0800

    利用xpath進行爬取

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
-driver = webdriver.Firefox('.')
 
-driver.get('http://rcuv.hkbs.org.hk/')
+driver = webdriver.Firefox()
+driver.get('https://www.bible.com/zh-TW/bible/46/EPH.2.CUNP')
+
+target=driver.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div/div/div/div[3]/span[2]/span[3]/span')
+print(target.text)
+
+driver.quit()
