d712d28bb5942ee25c50111a681d22870c98d8cd
Administrator <h3584935@gmail.com>
2018-03-06 18:48:36 +0800

    抓取到經文內容

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 driver = webdriver.Firefox()
 driver.get('https://www.bible.com/zh-TW/bible/46/EPH.2.CUNP')
 
-target=driver.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div/div/div/div[3]/span[2]/span[3]/span')
-print(target.text)
-
+divs = driver.find_elements_by_xpath("//div[@class='p']")
+print(divs[0].text)
+    
 driver.quit()
