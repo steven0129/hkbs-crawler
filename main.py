847114c77392e38c74ff175019ed7d289e4558db
Administrator <h3584935@gmail.com>
2018-03-07 13:44:40 +0800

    加入章數查詢

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import time
+import random
 from tqdm import tqdm
 
 options = Options()
@@ -9,9 +11,12 @@ books = ['GEN','EXO','LEV','NUM','DEU','JOS','JUG','RUT','1SA','2SA','1KI','2KI'
 driver = webdriver.Firefox(firefox_options=options, executable_path='./geckodriver')
 
 for book in tqdm(books):
-    tqdm.write('處理中經卷:' + book)
+    
+    driver.get('https://www.bible.com/zh-TW/bible/46/' + book)
+    chapterList = driver.find_element_by_xpath("//ul[@class='chapter-list']").find_elements_by_tag_name('a')
+    tqdm.write('處理中經卷: %s, 本卷書總章數: %d' % (book, len(chapterList)))
 
-    for i in tqdm(range(16)): # 章數
+    for i in tqdm(range(len(chapterList))): # 章數
         driver.get('https://www.bible.com/zh-TW/bible/46/'+ book +'.' + str(i+1) + '.CUNP')
         divs = driver.find_elements_by_xpath("//div[@class='p']")
 
@@ -20,6 +25,7 @@ for book in tqdm(books):
             for verse in tqdm(verses): # 節數
                 spans = verse.find_elements_by_class_name('add')
                 for span in spans: # 虛點點文字
-                    tqdm.write(verse.get_attribute('class') + ', ' + span.text)
+                    pass
+                    # tqdm.write(verse.get_attribute('class') + ', ' + span.text)
 
 driver.quit()
