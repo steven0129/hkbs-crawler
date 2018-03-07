f216d6b31c82aed2f554d648b9fec80204bb150a
Administrator <h3584935@gmail.com>
2018-03-07 13:55:38 +0800

    加入新約

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from tqdm import tqdm
 options = Options()
 options.add_argument('--headless')
 
-books = ['GEN','EXO','LEV','NUM','DEU','JOS','JUG','RUT','1SA','2SA','1KI','2KI','1CH','2CH','EZR','NEH','EST','JOB','PSM','PRO','ECC','SON','ISA','JER','LAM','EZE','DAN','HOS','JOE','AMO','OBA','JON','MIC','NAH','HAB','ZEP','HAG','ZEC','MAL']
+oldBooks = ['GEN','EXO','LEV','NUM','DEU','JOS','JUG','RUT','1SA','2SA','1KI','2KI','1CH','2CH','EZR','NEH','EST','JOB','PSM','PRO','ECC','SON','ISA','JER','LAM','EZE','DAN','HOS','JOE','AMO','OBA','JON','MIC','NAH','HAB','ZEP','HAG','ZEC','MAL']
+newBooks = ['MAT','MAK','LUK','JHN','ACT','ROM','1CO','2CO','GAL','EPH','PHL','COL','1TS','2TS','1TI','2TI','TIT','PHM','HEB','JAS','1PE','2PE','1JN','2JN','3JN','JUD','REV']
+books = oldBooks + newBooks
+
 driver = webdriver.Firefox(firefox_options=options, executable_path='./geckodriver')
 
 for book in tqdm(books):
@@ -20,9 +23,9 @@ for book in tqdm(books):
         driver.get('https://www.bible.com/zh-TW/bible/46/'+ book +'.' + str(i+1) + '.CUNP')
         divs = driver.find_elements_by_xpath("//div[@class='p']")
 
-        for div in divs:
+        for div in tqdm(divs):
             verses = div.find_elements_by_class_name('verse')
-            for verse in tqdm(verses): # 節數
+            for verse in verses: # 節數
                 spans = verse.find_elements_by_class_name('add')
                 for span in spans: # 虛點點文字
                     pass
