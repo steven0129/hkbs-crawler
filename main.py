aa79c3630658e715fc95292bf153e98ff14154d2
Administrator <h3584935@gmail.com>
2018-03-07 14:15:19 +0800

    Version 1 complete

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from tqdm import tqdm
+from pymongo import MongoClient
 import time
 import random
-from tqdm import tqdm
 
 options = Options()
 options.add_argument('--headless')
+client = MongoClient()
+db = client['uversion']
+collect = db['bibleLackTest']
 
 oldBooks = ['GEN','EXO','LEV','NUM','DEU','JOS','JUG','RUT','1SA','2SA','1KI','2KI','1CH','2CH','EZR','NEH','EST','JOB','PSM','PRO','ECC','SON','ISA','JER','LAM','EZE','DAN','HOS','JOE','AMO','OBA','JON','MIC','NAH','HAB','ZEP','HAG','ZEC','MAL']
 newBooks = ['MAT','MAK','LUK','JHN','ACT','ROM','1CO','2CO','GAL','EPH','PHL','COL','1TS','2TS','1TI','2TI','TIT','PHM','HEB','JAS','1PE','2PE','1JN','2JN','3JN','JUD','REV']
@@ -27,8 +31,8 @@ for book in tqdm(books):
             verses = div.find_elements_by_class_name('verse')
             for verse in verses: # 節數
                 spans = verse.find_elements_by_class_name('add')
+                verseNum = verse.get_attribute('class')
                 for span in spans: # 虛點點文字
-                    pass
-                    # tqdm.write(verse.get_attribute('class') + ', ' + span.text)
+                    collect.insert_one({'book': book, 'chap': (i+1), 'verse': verseNum, 'text': span.text})
 
 driver.quit()
